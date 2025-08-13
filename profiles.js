252e80a42c10fcf71baf45fb8498da34e9b78c40
kaybet35 <166719352+kaybet35@users.noreply.github.com>
2025-08-13 23:50:14 +0300

    first batch of profiles

--- profiles.js
+++ profiles.js
@@ -1,3 +1,27 @@
 document.profiles = [
     
+    {    
+        player: 'Sauldor',
+        x: .46,
+        y: -.1
+    },
+
+    {
+        player: 'kaybet',
+        x: .23,
+        y: -.24
+    },
+
+    {
+        player: 'Kapturek',
+        x: .09,
+        y: -.79
+    },
+    
+    {
+        player: 'Nielser',
+        x: .88,
+        y: .87
+    }
+
 ];
